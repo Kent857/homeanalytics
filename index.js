7276c24b731379e9d6a22e6726afa101d7bb2d8e
unknown <857068429@qq.com.com>
2021-09-15 14:24:49 +0800

    add start

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 
 app.get("/",function(req,res){
-    res.send("working")
+    res.send(Assignment_V19_MB_170521s.py)
 })
 
 app.listen(process.env.PORT || 5000)
